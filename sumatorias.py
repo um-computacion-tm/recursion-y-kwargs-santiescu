13fe1600104eff7c629d393565e42abe44c9b0fe
Santi Escudero <103152020+santiescu@users.noreply.github.com>
2024-04-22 13:03:15 -0300

    First commit

---
+++ sumatorias.py
@@ -0,0 +1,34 @@
+# def factorial (value):
+#     resultado = value 
+#     value -= 1
+#     while value > 1: 
+#          resultado = resultado * value
+#          value -= 1
+#          print ("resultado", resultado, value)
+#     return resultado
+
+# def factorial (value):
+#     if value in (0,1):
+#         return 1
+#     return value * factorial(value-1)     
+
+# prueba = factorial (5)
+# print (prueba)
+
+# def fibonacci (value):
+#     values = [0,1]
+#     for i in range(value - 1):
+#         values.append(values[-1] + values [-2])
+#         print (values)
+#     return values[value]
+
+# prueba = fibonacci (20)
+# print (prueba)
+
+# def escribir_el_nombre (**kwargs):
+#     print ("    inicio")
+
+
+
+
+
